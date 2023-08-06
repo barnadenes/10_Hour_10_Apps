4056336b3bd3f391ef5bb5b190f67cad1b371f06
Barna Denes <nyukk1007@gmail.com>
2023-08-06 09:53:37 +0200

    Questions

---
+++ QuizApp/index.js
@@ -0,0 +1,35 @@
+const QuizData = [
+    {
+        question: "How many meds am I taking?",
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "5",
+        correct: "c",
+    },
+    {
+        question: "Most used programming language?",
+        a: "English",
+        b: "Python",
+        c: "C#",
+        d: "JS",
+        correct: "d",
+    },
+    {
+        question: "President of the US?",
+        a: "Obama",
+        b: "Sleepy Joe",
+        c: "Trump",
+        d: "Kanye",
+        correct: "b",
+    },
+    {
+        question: "Who wrote the song + \"Mary on a Cross?\"",
+        a: "Nightwish",
+        b: "Lil'Pump",
+        c: "Ghost",
+        d: "Moikaloop",
+        correct: "c",
+    },
+
+]
