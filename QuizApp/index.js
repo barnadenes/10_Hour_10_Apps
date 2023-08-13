64c6bbeaeefba61d63aac7e81f6c272a941606c0
Barna Denes <nyukk1007@gmail.com>
2023-08-13 14:34:43 +0200

    Bug fest/ little bit a js update

--- QuizApp/index.js
+++ QuizApp/index.js
@@ -24,7 +24,7 @@ const QuizData = [
         correct: "b",
     },
     {
-        question: "Who wrote the song + \"Mary on a Cross?\"",
+        question: "Who wrote the song \"Mary on a Cross?\"",
         a: "Nightwish",
         b: "Lil'Pump",
         c: "Ghost",
@@ -33,3 +33,71 @@ const QuizData = [
     },
 
 ]
+
+let question = document.querySelector("#question-text");
+let a = document.querySelector("#qa");
+let b = document.querySelector("#qb");
+let c = document.querySelector("#qc");
+let d = document.querySelector("#qd");
+let correct = QuizData[1].correct;
+let i = 0;
+let inputSet = document.querySelectorAll('.input-check');
+const maxScore =  4;
+let score = 0;
+let isTrue = false;
+
+function init() {
+    question.innerHTML = QuizData[0].question;
+    a.innerHTML = QuizData[0].a;
+    b.innerHTML = QuizData[0].b;
+    c.innerHTML = QuizData[0].c;
+    d.innerHTML = QuizData[0].d;
+}
+
+function nextQuestion() {
+    isChecked(i);
+    if(isTrue === false) {
+        return;
+    }
+
+    question.innerHTML = QuizData[i].question;
+    a.innerHTML = QuizData[i].a;
+    b.innerHTML = QuizData[i].b;
+    c.innerHTML = QuizData[i].c;
+    d.innerHTML = QuizData[i].d;
+    i ++;
+}
+
+function isChecked(num) {
+    done();
+    let selected = undefined;
+
+    inputSet.forEach(input => {
+        if(input.checked === true) {
+            selected = input.id;
+            isTrue = true;
+            if(selected == QuizData[num].correct) {
+                score++;
+            }
+            else {
+                score = score;
+            }        
+        }
+    });
+
+    if(!isTrue) {
+        alert("Please select an option!");
+    }
+}
+
+function done() {
+    if(i == 4) {
+        question.innerHTML = `Your score is ${score} / ${maxScore} `;
+        document.querySelectorAll(".hide").style.display = "none";
+    }
+}
+
+init();
+
+
+
