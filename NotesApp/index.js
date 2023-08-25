5108aabbd6cf308da5e57524e6dfe505c1467c6b
Barna Denes <nyukk1007@gmail.com>
2023-08-25 20:38:58 +0200

    event listeners / tsuzuku...

--- NotesApp/index.js
+++ NotesApp/index.js
@@ -1,20 +1,49 @@
-const editBtn = document.querySelector('.edit');
-const deletetBtn = document.querySelector('.delete');
-const noteEl = document.querySelector('.notes');
-const mainEl = noteEl.querySelector('.main');
-const textareaEl = noteEl.querySelector('textarea');
+const addBtn = document.querySelector(".add");
 
+function createNote() {
+  const notes = document.createElement("div");
+  notes.classList.add("notes");
 
-editBtn.addEventListener('click', () => {
-    mainEl.classList.toggle('hidden');
-    textareaEl.classList.toggle('hidden');
-    const content = textareaEl.value
+  notes.innerHTML = `<div class="tools">
+    <button class="edit">
+      <i class="fas fa-edit"></i>
+    </button>
+    <button class="delete">
+      <i class="fas fa-trash"></i>
+    </button>
+  </div>
+  <div class="main hidden"></div>
+  <textarea></textarea>`;
+
+  document.body.appendChild(notes);
+
+  let editBtn = document.querySelector(".edit");
+  let deletetBtn = document.querySelector(".delete");
+  let mainEl = notes.querySelector(".main");
+  let textareaEl = notes.querySelector("textarea");
+
+  editBtn.addEventListener("click", () => {
+    mainEl.classList.toggle("hidden");
+    textareaEl.classList.toggle("hidden");
+    const content = textareaEl.value;
 
     mainEl.innerHTML = marked.parse(content);
-})
+  });
 
-textareaEl.addEventListener('click', (e) => {
-    const {value} = e.target;
+  notes.addEventListener("click", (e) => {
+    if(e.target.classList.contains('fa-trash')) {
+        notes.remove();
+    }
+    
+  });
+
+  textareaEl.addEventListener("click", (e) => {
+    const { value } = e.target;
 
     mainEl.innerHTML = marked.parse(value);
-})
+  });
+}
+
+addBtn.addEventListener("click", () => {
+  createNote();
+});
