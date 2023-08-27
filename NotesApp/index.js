9f865636b962b9397a41828754ad84f52124c4f9
Barna Denes <nyukk1007@gmail.com>
2023-08-27 19:50:23 +0200

    local storage added/ needs so tweaking

--- NotesApp/index.js
+++ NotesApp/index.js
@@ -1,6 +1,8 @@
 const addBtn = document.querySelector(".add");
+const LSData = localStorage.getItem("text");
 
-function createNote() {
+//CREATE NOTE
+function createNote(text = "") {
   const notes = document.createElement("div");
   notes.classList.add("notes");
 
@@ -12,30 +14,76 @@ function createNote() {
       <i class="fas fa-trash"></i>
     </button>
   </div>
-  <div class="main hidden"></div>
-  <textarea></textarea>`;
+  <div class="main hidden">${text}</div>
+  <textarea>${text}</textarea>`;
 
   document.body.appendChild(notes);
 
-  let mainEl = notes.querySelector(".main");
-  let textareaEl = notes.querySelector("textarea");
+  const mainEl = notes.querySelector(".main");
+  const textareaEl = notes.querySelector("textarea");
+  addLS(textareaEl.value);
 
+  // REMOVE
   notes.addEventListener("click", (e) => {
     if (e.target.classList.contains("fa-trash")) {
       notes.remove();
+      removeLS(notes);
+
+      // EDIT-PARSE
     } else if (e.target.classList.contains("fa-edit")) {
       const content = textareaEl.value;
       mainEl.classList.toggle("hidden");
       textareaEl.classList.toggle("hidden");
+      addLS(content);
 
       mainEl.innerHTML = marked.parse(content);
       if (content === null || content === undefined) {
         return "";
       }
-    } 
+    }
   });
 }
 
+// ADD NOTE
 addBtn.addEventListener("click", () => {
-  createNote();
+  createNote((text = ""));
 });
+
+// GET
+function getLS() {
+  const textArr = JSON.parse(localStorage.getItem("text"));
+
+  return textArr === null  ? [] : textArr;
+}
+
+// ADD
+function addLS(newText) {
+  const items = getLS();
+
+  if (items && !items.includes(newText)) {
+    localStorage.setItem("text", JSON.stringify([...items, newText]));
+  } else {
+    return "";
+  }
+}
+
+// DEL
+function removeLS(remove) {
+  const textArr = getLS();
+
+  localStorage.setItem(
+    "text",
+    JSON.stringify(textArr.filter((id) => id !== remove))
+  );
+}
+
+// LOAD LAST SESSION
+function preLoad() {
+  const localText = getLS();
+
+  localText.forEach((text) => {
+    createNote(text);
+  });
+}
+
+preLoad();
