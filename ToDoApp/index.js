5522270350887eed2ba37e6d41121489bd07db30
Barna Denes <nyukk1007@gmail.com>
2023-08-30 05:20:46 +0200

    done/ few tweeks could be added later

--- ToDoApp/index.js
+++ ToDoApp/index.js
@@ -1,20 +1,52 @@
-const TDListEl = document.querySelector('.todo-list');
-const inputEl = document.querySelector('#todo-input');
-const formEl = document.querySelector('.formEl');
-const ULEl = TDListEl.querySelector('ul');
+const TDListEl = document.querySelector(".todo-list");
+const inputEl = document.querySelector("#todo-input");
+const formEl = document.querySelector(".formEl");
+const ULEl = TDListEl.querySelector("ul");
 
-formEl.addEventListener('submit', (e) => {
+formEl.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const inputText = inputEl.value;
+  addToDo();
+});
 
-  if(inputText) {
-    const liEl = document.createElement('li');
-    const hrEl = document.createElement('hr');
+function addToDo() {
+  const inputText = inputEl.value;
+  const liEl = document.createElement("li");
+  const hrEl = document.createElement("hr");
+
+  if (inputText) {
     liEl.innerText = inputText;
     ULEl.appendChild(liEl);
     ULEl.appendChild(hrEl);
+
+    inputEl.value = "";
+
+    liEl.addEventListener("click", () => {
+      liEl.classList.toggle("cross");
+      updateLS();
+    });
+
+    liEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      liEl.remove();
+      hrEl.remove();
+      updateLS();
+    });
   }
+  updateLS();
+}
 
-})
+function updateLS() {
+  const notesEl = document.querySelectorAll('li');
+  const notes = [];
 
+  notesEl.forEach((note) => {
+    notes.push({
+      text: note.innerText,
+      completed: note.classList.contains('cross')
+    });
+  });
+
+  localStorage.setItem('notes', JSON.stringify(notes));
+}
