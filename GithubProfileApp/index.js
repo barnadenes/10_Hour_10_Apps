25e401444f76b19ef7bb32da5a468aa6e7c9af2b
Barna Denes <nyukk1007@gmail.com>
2023-08-31 07:33:38 +0200

    sort repos by stars /done

--- GithubProfileApp/index.js
+++ GithubProfileApp/index.js
@@ -1,29 +1,27 @@
-const APIURL = 'https://api.github.com/users/';
-const mainEL = document.querySelector('.main');
-const formEl = document.querySelector('#form');
-const inputEl = document.querySelector('#search');
+const APIURL = "https://api.github.com/users/";
+const mainEL = document.querySelector(".main");
+const formEl = document.querySelector("#form");
+const inputEl = document.querySelector("#search");
 
-getUser('florinpop17');
+getUser("florinpop17");
 
 async function getUser(name) {
-    const resp = await fetch(APIURL + name);
-    const respData  = await resp.json();
+  const resp = await fetch(APIURL + name);
+  const respData = await resp.json();
 
-    createUserCard(respData);
-    fetchRepos(name);
-
+  createUserCard(respData);
+  fetchRepos(name);
 }
 
 async function fetchRepos(name) {
-    const resp = await fetch(APIURL + name + '/repos');
-    const response = await resp.json();
+  const resp = await fetch(APIURL + name + "/repos");
+  const response = await resp.json();
 
-    addReposeToCard(response);
+  addReposeToCard(response);
 }
 
 function createUserCard(user) {
-
-    const card = `
+  const card = `
         <div class="img-container">
             <img class="avatar" src="${user.avatar_url}" alt="${user.name}"/>
         </div>
@@ -40,30 +38,33 @@ function createUserCard(user) {
         </div>
     `;
 
-    mainEL.innerHTML = card;
+  mainEL.innerHTML = card;
 }
 
 function addReposeToCard(repos) {
-    const reposEL = document.querySelector('.repos');
-    
-    repos.forEach(repo => {
-        const aEl = document.createElement('a');
-        aEl.classList.add('repo');
-        aEl.href = repo.html_url;
-        aEl.target = '_blank';
+  const reposEL = document.querySelector(".repos");
 
-        aEl.innerText = repo.name;
-        reposEL.appendChild(aEl);
-    })
+  repos
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 12)
+    .forEach((repo) => {
+      const aEl = document.createElement("a");
+      aEl.classList.add("repo");
+      aEl.href = repo.html_url;
+      aEl.target = "_blank";
+
+      aEl.innerText = repo.name;
+      reposEL.appendChild(aEl);
+    });
 }
 
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = inputEl.value;
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = inputEl.value;
 
-    if(name) {
-        getUser(name);
-    }
+  if (name) {
+    getUser(name);
+  }
 
-    inputEl.value = '';
-})
+  inputEl.value = "";
+});
